2e1c46df3054db79516b10ec54d6f0fef9cb9130
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-08 11:06:14 -0800

    Add dynamic drawing based on song amplitude

--- sketch.js
+++ sketch.js
@@ -1,18 +1,21 @@
 let song;
 let sliderRate;
 let button;
+let amp;
 
 function setup() {
   createCanvas(200,200);
   song = loadSound("assets/coalesce.mp3", loaded);
+  song.setVolume(0.3);
+  amp = new p5.Amplitude();
+  console.log(amp);
+}
+
+function loaded() {
   button = createButton("play");
   button.mousePressed(togglePlaying);
   song.setVolume(0.5);
   sliderRate = createSlider(0.5,1.5,1,0.01);
-}
-
-function loaded() {
-  console.log("song loaded");
 }
 
 function togglePlaying() {
@@ -26,6 +29,12 @@ function togglePlaying() {
 }
 
 function draw() {
-  background(0);
-  song.rate(sliderRate.value());
+  background(40);
+  let vol = amp.getLevel();
+  let diam = map(vol, 0, 0.5, 10, 200);
+  fill(0, 200, 255);
+  ellipse(width/2, height/2, diam, diam)
+  if(song.currentTime() > 1) {
+    song.rate(sliderRate.value());
+  }
 }
