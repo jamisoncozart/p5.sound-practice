5ba25911f7b0f86d7676940ca9448ab00dd8c848
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-08 14:43:26 -0800

    Add README.md

---
+++ README.md
@@ -0,0 +1,42 @@
+# _p5.sound Library Practice_
+
+#### _See the frequency spectrum of an mp3 sound, and speed up or slow down the playback._, 8 February 2020
+
+#### By _**Jamison Cozart**_
+
+## Description
+
+_Simple site to explore a few of the functionalities of the p5.js library, and their associated p5.sound library._
+
+## Prerequisites
+
+*Required:* Live-server plugin for VSCode or other text editors
+
+## Setup/Installation Requirements
+
+1. Clone the repository into preferred directory:
+    ```
+    git clone https://github.com/jamisoncozart/p5.sound-practice
+    ```
+2. Change into project directory:
+    ```
+    cd p5.sound-practice
+    ```
+2. Open working directory in Visual Studio Code or preferred text editor:
+    ```
+    code .
+    ```
+3. Open `index.html` using the `open with live-server` command or `right-click` on the `index.html` and click `open with live-server`.
+
+## Technologies Used
+
+* HTML
+* Javascript
+* p5.js
+* Git
+
+### License
+
+*This software is licensed under the MIT license*
+
+Copyright (c) 2020 **_Jamison Cozart_**
--- sketch.js
+++ sketch.js
@@ -28,56 +28,59 @@ function togglePlaying() {
 
 function draw() {
   background(60);
-  let spectrum = fft.analyze();
-  let bass = fft.getEnergy(20, 80);
-  let lowMid = fft.getEnergy(80, 200);
-  let mid = fft.getEnergy(200, 500);
-  let speach = fft.getEnergy(500, 800);
-  let highMid = fft.getEnergy(800, 1400);
-  let lowHigh = fft.getEnergy(1400, 2500);
-  let high = fft.getEnergy(2500, 8000);
-  let highHigh = fft.getEnergy(8000, 20000);
-  
   noStroke();
   fill(0, 255, 255);
-  if(bass > 100) {
-    let bassh = -height + map(bass, 101, 255, height, 0);
-    rect(10, height, 40, bassh);
-  }
-  if(lowMid > 100) {
-    let lMh = -height + map(lowMid, 101, 255, height, 0);
-    rect(60, height, 40, lMh);
-  }
-  if(mid > 100) {
-    let midh = -height + map(mid, 101, 255, height, 0);
-    rect(110, height, 40, midh);
-  }
-  if(speach > 100) {
-    let speachh = -height + map(speach, 101, 255, height, 0);
-    rect(160, height, 40, speachh);
-  }
-  if(highMid > 100) {
-    let hmh = -height + map(highMid, 101, 255, height, 0);
-    rect(210, height, 40, hmh);
-  }
-  if(lowHigh > 100) {
-    let lhh = -height + map(lowHigh, 101, 255, height, 0);
-    rect(260, height, 40, lhh);
-  }
-  if(high > 100) {
-    let highh = -height + map(high, 101, 255, height, 0);
-    rect(310, height, 40, highh);
-  }
-  if(highHigh > 100) {
-    let hhh = -height + map(highHigh, 101, 255, height, 0);
-    rect(360, height, 40, hhh);
-  }
-
-  // for (var i = 0; i< spectrum.length; i++){
-  //   let x = map(i, 0, spectrum.length, 0, width);
-  //   let h = -height + map(spectrum[i], 0, 255, height, 0);
-  //   rect(x, height, width / spectrum.length, h );
+  //---------------------------------------------------------
+  //CUSTOM FREQUENCY BARS
+  // let bass = fft.getEnergy(20, 80);
+  // let lowMid = fft.getEnergy(80, 200);
+  // let mid = fft.getEnergy(200, 500);
+  // let speach = fft.getEnergy(500, 800);
+  // let highMid = fft.getEnergy(800, 1400);
+  // let lowHigh = fft.getEnergy(1400, 2500);
+  // let high = fft.getEnergy(2500, 8000);
+  // let highHigh = fft.getEnergy(8000, 20000);
+  // if(bass > 100) {
+  //   let bassh = -height + map(bass, 101, 255, height, 0);
+  //   rect(10, height, 40, bassh);
   // }
+  // if(lowMid > 100) {
+  //   let lMh = -height + map(lowMid, 101, 255, height, 0);
+  //   rect(60, height, 40, lMh);
+  // }
+  // if(mid > 100) {
+  //   let midh = -height + map(mid, 101, 255, height, 0);
+  //   rect(110, height, 40, midh);
+  // }
+  // if(speach > 100) {
+  //   let speachh = -height + map(speach, 101, 255, height, 0);
+  //   rect(160, height, 40, speachh);
+  // }
+  // if(highMid > 100) {
+  //   let hmh = -height + map(highMid, 101, 255, height, 0);
+  //   rect(210, height, 40, hmh);
+  // }
+  // if(lowHigh > 100) {
+  //   let lhh = -height + map(lowHigh, 101, 255, height, 0);
+  //   rect(260, height, 40, lhh);
+  // }
+  // if(high > 100) {
+  //   let highh = -height + map(high, 101, 255, height, 0);
+  //   rect(310, height, 40, highh);
+  // }
+  // if(highHigh > 100) {
+  //   let hhh = -height + map(highHigh, 101, 255, height, 0);
+  //   rect(360, height, 40, hhh);
+  // }
+                  
+  // --------------------------------------------------------
+  // FULL SPECTRUM ANALYZER
+  let spectrum = fft.analyze();
+  for (var i = 0; i< spectrum.length; i++){
+    let x = map(i, 0, spectrum.length, 0, width);
+    let h = -height + map(spectrum[i], 0, 255, height, 0);
+    rect(x, height, width / spectrum.length, h );
+  }
   if(song.currentTime() > 1) {
     song.rate(sliderRate.value());
   }
